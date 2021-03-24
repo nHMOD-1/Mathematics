45ec43a1bc5a18be4f2672ffeabd4fdcd1df96bc
nHMOD-1 <78529253+nHMOD-1@users.noreply.github.com>
2021-03-24 17:31:37 +0300

    Update A21.java

--- عرض برزنتيشن/src/A21.java
+++ عرض برزنتيشن/src/A21.java
@@ -1,9 +1,14 @@
-
 public class A21 {
-
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+ 
+    // عند إستدعاءها نعطيها عددين فترجع ناتج جمع هذين العددين sum هنا قمنا بتعريف دالة إسمها
+    public static int sum(int a, int b) {
+        return a+b;
     }
-
+ 
+ 
+    public static void main(String[] args) {
+        // لحساب ناتج جمع العددين 5 و 10 sum هنا قمنا باستدعاء الدالة
+        System.out.println( "5 + 10 = " + sum(10, 5) );
+    }
+ 
 }
